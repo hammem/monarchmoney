bbd68f59be03ec4127993643cfd09a8052cec51b
hammem <hammem@localhost.local>
2024-02-10 15:34:34 -0500

    Get started on creating unit tests

---
+++ tests/test_monarchmoney.py
@@ -0,0 +1,58 @@
+import os
+import pickle
+import unittest
+from unittest.mock import patch
+
+import json
+from gql import Client
+from monarchmoney import MonarchMoney
+
+
+class TestMonarchMoney(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        """
+        Set up any necessary data or variables for the tests here.
+        This method will be called before each test method is executed.
+        """
+        with open("temp_session.pickle", "wb") as fh:
+            session_data = {
+                "cookies": {"test_cookie": "test_value"},
+                "token": "test_token",
+            }
+            pickle.dump(session_data, fh)
+        self.monarch_money = MonarchMoney()
+        self.monarch_money.load_session("temp_session.pickle")
+
+    @patch.object(Client, "execute_async")
+    async def test_get_transactions_summary(self, mock_execute_async):
+        """
+        Test the get_transactions_summary method.
+        """
+        mock_execute_async.return_value = TestMonarchMoney.loadTestData(
+            filename="get_transactions_summary.json",
+        )
+
+        result = await self.monarch_money.get_transactions_summary()
+        self.assertIsNotNone(result, "Expected result to not be None")
+        self.assertEqual(
+            result["aggregates"]["summary"]["sumIncome"],
+            50000,
+            "Expected sumIncome to be 50000",
+        )
+
+    @classmethod
+    def loadTestData(cls, filename) -> dict:
+        filename = f"{os.path.dirname(os.path.realpath(__file__))}/{filename}"
+        with open(filename, "r") as file:
+            return json.load(file)
+
+    def tearDown(self):
+        """
+        Tear down any necessary data or variables for the tests here.
+        This method will be called after each test method is executed.
+        """
+        os.remove("temp_session.pickle")
+
+
+if __name__ == "__main__":
+    unittest.main()
