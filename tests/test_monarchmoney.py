4a4d21de50d4a7879d79c5541902c8ab4e58fbe1
Calvin Chan <calvinchancan@gmail.com>
2024-03-07 00:58:30 -0500

    add test for delete_account

--- tests/test_monarchmoney.py
+++ tests/test_monarchmoney.py
@@ -39,6 +39,32 @@ class TestMonarchMoney(unittest.IsolatedAsyncioTestCase):
             "Expected sumIncome to be 50000",
         )
 
+    @patch.object(Client, "execute_async")
+    async def test_delete_account(self, mock_execute_async):
+        """
+        Test the delete_account method.
+        """
+
+        mock_execute_async.return_value = {
+            "deleteAccount": {
+                "deleted": True,
+                "errors": None,
+                "__typename": "DeleteAccountMutation",
+            }
+        }
+
+        result = await self.monarch_money.delete_account("170123456789012345")
+
+        mock_execute_async.assert_called_once()
+
+        kwargs = mock_execute_async.call_args.kwargs
+        self.assertEqual(kwargs["operation_name"], "Common_DeleteAccount")
+        self.assertEqual(kwargs["variable_values"], {"id": "170123456789012345"})
+
+        self.assertIsNotNone(result, "Expected result to not be None")
+        self.assertEqual(result["deleteAccount"]["deleted"], True)
+        self.assertEqual(result["deleteAccount"]["errors"], None)
+
     @classmethod
     def loadTestData(cls, filename) -> dict:
         filename = f"{os.path.dirname(os.path.realpath(__file__))}/{filename}"
